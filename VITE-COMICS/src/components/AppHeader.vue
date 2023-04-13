49e197145b78d2c4ae0d589e97d6bf06bf6d6b9a
JorgeLuix <tec.jorge.29@gmai.com>
2023-04-13 16:23:39 +0200

    lavorando Appheader

--- VITE-COMICS/src/components/AppHeader.vue
+++ VITE-COMICS/src/components/AppHeader.vue
@@ -1,9 +1,36 @@
 <template>
     <header class="d-flex justify-content-center ">
         <img src="../assets/img/dc-logo.png" alt="logoDc">
-        <ul>
+        <ul class="d-flex gap-4 text-uppercase align-items-center ">
             <li>
-                {{ lista }}
+                characters
+            </li>
+            <li>
+                comics
+            </li>
+            <li>
+                movies
+            </li>
+            <li>
+                tv
+            </li>
+            <li>
+                games
+            </li>
+            <li>
+                collectibiles
+            </li>
+            <li>
+                videos
+            </li>
+            <li>
+                fans
+            </li>
+            <li>
+                news
+            </li>
+            <li>
+                shop
             </li>
         </ul>
     </header>
@@ -14,7 +41,7 @@
         name: 'DcComics',
         data(){
             return{
-                lista:'listaNav',
+                
             }
         }
     }
